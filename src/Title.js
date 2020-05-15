ed4451ca1898b5013a79d098056b7b116923b332
Sviatlana Aplachka <svetlana.aplachka@gmail.com>
2020-05-15 09:42:07 +0300

    task2

---
+++ src/Title.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, Checkbox } from '@material-ui/core';
+
+const styles = {
+    root: {
+        display: "flex",
+    },
+    h3: {
+        flexGrow: 1
+    }
+
+};
+
+const Title = (props) => {
+
+    const { classes } = props;
+
+    return (<div className={classes.root} >
+        <h3 className={classes.h3} >Task 2</h3>
+        <Checkbox
+            color='primary'
+            onChange={props.handler}
+        /></div>);
+}
+Title.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(Title);
